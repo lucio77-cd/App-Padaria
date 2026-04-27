d545f408cf8919eb2408c16a642e5fb4e319d13b
lucio77-cd <luciosantoscarvalho@gmail.com>
2026-04-27 07:49:49 -0300

    Add files via upload
    Primeiro upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBn1-bjBSfJAayZcifjY9DiJazzcyiQK58",
+  authDomain: "padariaapp-e025f.firebaseapp.com",
+  projectId: "padariaapp-e025f",
+  storageBucket: "padariaapp-e025f.firebasestorage.app",
+  messagingSenderId: "242006234958",
+  appId: "1:242006234958:web:c87d401f09ba173e27b01f"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
